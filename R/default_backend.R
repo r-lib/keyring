528e0f00a4a73d8d1ce92d2625bb430277d79e0e
Gábor Csárdi <csardi.gabor@gmail.com>
2017-02-12 11:00:01 +0000

    Default backend: secret service on Linux, man page

--- R/default_backend.R
+++ R/default_backend.R
@@ -1,19 +1,28 @@
 
 #' Select the default backend for a platform
 #'
-#' TODO
+#' 1. On Windows, the Windows Credential Store is used.
+#' 1. On macOS, Keychain services are selected.
+#' 1. Linux uses the Secret Service API.
+#' 1. On other operating systems, secrets are stored in environment
+#'   variables.
 #'
 #' @export
-#' @name backends
+#' @name keyring backends
 #' @examples
-#' # TODO
 
 default_backend <- function() {
   sysname <- tolower(Sys.info()[["sysname"]])
+
   if (sysname == "windows") {
     backend_wincred()
+
   } else if (sysname == "darwin") {
     backend_macos()
+
+  } else if (sysname == "linux") {
+    backend_secret_service()
+
   } else {
     warning("Selecting ", sQuote(env), " backend. ",
             "Secrets are stored in environment variables")
