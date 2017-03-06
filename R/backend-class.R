3836e1195f302bf6247413fe445f6d53d2d8fdc5
Gábor Csárdi <csardi.gabor@gmail.com>
2017-03-06 10:41:39 +0000

    Base classes, env backend

---
+++ R/backend-class.R
@@ -0,0 +1,58 @@
+
+abstract_method <- function() {
+  stop("An abstract keyring method is called. This is an internal error. ",
+       "It most likely happends because of a broken keyring backend that ",
+       "does not implement all keyring functions.")
+}
+
+#' @importFrom R6 R6Class
+#' @export
+
+backend <- R6Class(
+  "backend",
+  public = list(
+    has_keyring_support = function() FALSE,
+
+    get = function(service, username = NULL)
+      abstract_method(),
+    set = function(service, username = NULL)
+      abstract_method(),
+    set_with_value = function(service, username = NULL, password = NULL)
+      abstract_method(),
+    delete = function(service, username)
+      abstract_method()
+  )
+)
+
+#' @export
+
+backend_keyrings <- R6Class(
+  "backend_keyrings",
+  inherit = backend,
+  public = list(
+    has_keyring_support = function() TRUE,
+
+    get = function(service, username = NULL, keyring = NULL)
+      abstract_method(),
+    set = function(service, username = NULL, keyring = NULL)
+      abstract_method(),
+    set_with_value = function(service, username = NULL, password = NULL,
+      keyring = NULL)
+      abstract_method(),
+    delete = function(service, username)
+      abstract_method(),
+    list = function(service, keyring = NULL)
+      abstract_method(),
+
+    keyring_create = function(keyring) abstract_method(),
+    keyring_list = function() abstract_method(),
+    keyring_delete = function(keyring = NULL) abstract_method(),
+    keyring_lock = function(keyring = NULL) abstract_method(),
+    keyring_unlock = function(keyring = NULL) abstract_method(),
+    keyring_default = function() abstract_method(),
+    keyring_set_default = function() abstract_method()
+  ),
+  private = list(
+    default_keyring = NULL
+  )
+)
