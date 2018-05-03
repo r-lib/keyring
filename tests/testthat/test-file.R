0c2e846b044e835f93f65a7061fdc41005b72cc3
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-05-03 18:08:06 +0200

    add simple tests for file-backend

--- tests/testthat/test-file.R
+++ tests/testthat/test-file.R
@@ -10,9 +10,13 @@ test_that("specify keyring explicitly", {
 
   kb <- backend_file$new(keyring = keyring)
 
-  expect_silent(
-    kb$set_with_value(service, username, password)
-  )
+  expect_true(kb$keyring_is_locked())
+  expect_silent(kb$keyring_unlock(NULL, random_password()))
+  expect_false(kb$keyring_is_locked())
+
+  expect_silent(kb$set_with_value(service, username, password))
 
   expect_equal(kb$get(service, username), password)
+
+  expect_silent(kb$keyring_delete())
 })
