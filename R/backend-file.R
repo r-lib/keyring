9cea2eb8e365badd4b164c66ac03824a1b9a3b83
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2018-05-04 17:24:26 +0200

    introduce key/nonce consistency check for file keyrings

--- R/backend-file.R
+++ R/backend-file.R
@@ -39,7 +39,7 @@ backend_file <- R6Class(
       b_file_keyring_delete(self, private, keyring),
 
     keyring_lock = function(keyring = NULL)
-      b_file_keyring_lock(self, private),
+      b_file_keyring_lock(self, private, keyring),
     keyring_unlock = function(keyring = NULL, password = NULL)
       b_file_keyring_unlock(self, private, keyring, password),
     keyring_is_locked = function(keyring = NULL)
@@ -56,12 +56,14 @@ backend_file <- R6Class(
     key = NULL,
     nonce = NULL,
     items = NULL,
+    check = NULL,
     keyring_file = function(name = NULL)
       b_file_keyring_file(self, private, name),
     keyring_read_file = function(name = NULL)
       b_file_read_keyring_file(self, private, name),
-    keyring_write_file = function(name = NULL, nonce = NULL, items = NULL)
-      b_file_write_keyring_file(self, private, name, nonce, items),
+    keyring_write_file = function(name = NULL, nonce = NULL, items = NULL,
+      key = NULL)
+      b_file_write_keyring_file(self, private, name, nonce, items, key),
     keyring_set = function(keyring = NULL)
       b_file_keyring_set(self, private, keyring),
     keyring_get = function()
@@ -78,6 +80,10 @@ backend_file <- R6Class(
       b_file_items_get(self, private, keyring),
     items_set = function(items = NULL)
       b_file_items_set(self, private, items),
+    check_get = function(keyring = NULL)
+      b_file_check_get(self, private, keyring),
+    check_set = function(check = NULL)
+      b_file_check_set(self, private, check),
     secret_encrypt = function(secret, nonce = NULL, key = NULL)
       b_file_secret_encrypt(self, private, secret, nonce, key),
     secret_decrypt = function(secret, nonce = NULL, key = NULL)
@@ -91,6 +97,8 @@ b_file_init <- function(self, private, keyring) {
 }
 
 b_file_get <- function(self, private, service, username, keyring) {
+
+  self$keyring_unlock(keyring)
 
   all_items <- private$items_get(keyring)
   item_matches <- sapply(all_items, `[[`, "service_name") %in% service
@@ -118,6 +126,9 @@ b_file_set <- function(self, private, service, username, keyring) {
 
 b_file_set_with_value <- function(self, private, service, username,
                                   password, keyring) {
+
+
+  self$keyring_unlock(keyring)
 
   all_items <- private$items_get(keyring)
 
@@ -157,27 +168,59 @@ b_file_keyring_create <- function(self, private, keyring, nonce, items) {
 }
 
 b_file_keyring_delete <- function(self, private, keyring) {
-  unlink(private$keyring_file(keyring %||% private$keyring))
+
+  self$keyring_unlock(keyring)
+
+  unlink(private$keyring_file(keyring))
+
   invisible(self)
 }
 
-b_file_keyring_lock <- function(self, private) {
+b_file_keyring_lock <- function(self, private, keyring) {
+
+  assert_that(file.exists(private$keyring_file(keyring)))
+
   private$key <- NULL
+
   invisible(self)
 }
 
 b_file_keyring_unlock <- function(self, private, keyring, password) {
-  if (!is.null(keyring))
-    self$keyring_set_default(keyring)
-  private$key_set(password)
+
+  if (is.null(private$key))
+    private$key_set(password)
+
+  assert_that(file.exists(private$keyring_file(keyring)))
+
+  if (self$keyring_is_locked(keyring))
+    private$key_set(password)
+
+  if (self$keyring_is_locked(keyring)) {
+    private$key <- NULL
+    b_file_error("failed to unlock keyring")
+  }
+
   invisible(self)
 }
 
 b_file_keyring_is_locked <- function(self, private, keyring) {
-  if (!is.null(keyring))
-    normalizePath(keyring) != private$keyring || is.null(private$key)
-  else
-    is.null(private$key)
+
+  if (is.null(private$key)) {
+    TRUE
+  } else {
+    tryCatch(
+      {
+        private$secret_decrypt(private$check_get(keyring))
+        FALSE
+      },
+      error = function(e) {
+        if(conditionMessage(e) == "Failed to decrypt")
+          TRUE
+        else
+          signalCondition(e)
+      }
+    )
+  }
 }
 
 b_file_keyring_default <- function(self, private) {
@@ -212,18 +255,28 @@ b_file_read_keyring_file <- function(self, private, name) {
 
   yml <- yaml::yaml.load_file(file_name)
 
+  assert_that(is.list(yml),
+              assertthat::has_name(yml, "keyring_info"),
+              is.list(yml[["keyring_info"]]),
+              assertthat::has_name(yml[["keyring_info"]], "nonce"),
+              assertthat::has_name(yml[["keyring_info"]], "integrity_check"),
+              assertthat::has_name(yml, "items"),
+              is.list(yml[["items"]]))
+
   list(
     nonce = sodium::hex2bin(yml[["keyring_info"]][["nonce"]]),
-    items = yml[["items"]]
+    items = yml[["items"]],
+    check = paste(
+      strsplit(yml[["keyring_info"]][["integrity_check"]], "\n")[[1L]],
+      collapse = ""
+    )
   )
 }
 
-b_file_write_keyring_file <- function(self, private, keyring, nonce, items) {
+b_file_write_keyring_file <- function(self, private, keyring, nonce, items,
+  key) {
 
-  nonce <- nonce %||% private$nonce
-
-  if (is.raw(nonce))
-    nonce <- sodium::bin2hex(nonce)
+  nonce <- nonce %||% private$nonce_get(keyring)
 
   yaml::write_yaml(
     list(
@@ -231,7 +284,12 @@ b_file_write_keyring_file <- function(self, private, keyring, nonce, items) {
         keyring_version = as.character(
           utils::packageVersion(methods::getPackageName())
         ),
-        nonce = nonce
+        nonce = sodium::bin2hex(nonce),
+        integrity_check = private$secret_encrypt(
+          paste(sample(letters, 24L, replace = TRUE), collapse = ""),
+          nonce,
+          key
+        )
       ),
       items = items %||% private$items
     ),
@@ -245,12 +303,14 @@ b_file_keyring_set <- function(self, private, keyring) {
   kr <- private$keyring_read_file(keyring)
   private$nonce_set(kr[["nonce"]])
   private$items_set(kr[["items"]])
+  private$check_set(kr[["check"]])
 }
 
 b_file_keyring_get <- function(self, private) {
   list(
     nonce = private$nonce_get(),
-    items = private$items_get()
+    items = private$items_get(),
+    check = private$check_get()
   )
 }
 
@@ -311,15 +371,33 @@ b_file_items_get <- function(self, private, keyring) {
     items <- private$items
   }
 
-  lapply(items, b_file_check_item)
+  lapply(items, b_file_validate_item)
 }
 
 b_file_items_set <- function(self, private, items) {
 
   private$items <- lapply(
     items %||% private$keyring_read_file()[["items"]],
-    b_file_check_item
+    b_file_validate_item
   )
+
+  invisible(self)
+}
+
+b_file_check_get <- function(self, private, keyring) {
+
+  if (!is.null(keyring)) {
+    private$keyring_read_file(keyring)[["check"]]
+  } else {
+    if (is.null(private$check))
+      private$check_set()
+    private$check
+  }
+}
+
+b_file_check_set <- function(self, private, check) {
+
+  private$check <- check %||% private$keyring_read_file()[["check"]]
 
   invisible(self)
 }
@@ -356,7 +434,7 @@ b_file_error <- function(problem, reason = NULL) {
   stop("keyring error (file-based keyring), ", info, call. = FALSE)
 }
 
-b_file_check_item <- function(item) {
+b_file_validate_item <- function(item) {
 
   assert_that(is.list(item), length(item) == 3L,
               assertthat::has_name(item, "service_name"),
--- tests/testthat/test-file.R
+++ tests/testthat/test-file.R
@@ -11,12 +11,39 @@ test_that("specify keyring explicitly", {
   kb <- backend_file$new(keyring = keyring)
 
   expect_true(kb$keyring_is_locked())
-  expect_silent(kb$keyring_unlock(NULL, random_password()))
+  expect_silent(kb$keyring_unlock(password = random_password()))
   expect_false(kb$keyring_is_locked())
 
   expect_silent(kb$set_with_value(service, username, password))
 
   expect_equal(kb$get(service, username), password)
 
+  expect_silent(kb$keyring_delete())
+})
+
+test_that("key consistency check", {
+
+  service <- random_service()
+  username <- random_username()
+  password <- random_password()
+  keyring <- random_keyring()
+  keyring_pwd_1 <- random_password()
+  keyring_pwd_2 <- random_password()
+
+  kb <- backend_file$new(keyring = keyring)
+
+  expect_silent(kb$keyring_unlock(password = keyring_pwd_1))
+  expect_silent(kb$set_with_value(service, username, password))
+
+  expect_silent(kb$keyring_unlock(password = keyring_pwd_2))
+  expect_silent(kb$keyring_lock())
+
+  expect_error(kb$keyring_unlock(password = keyring_pwd_2),
+               "failed to unlock keyring")
+
+  kb$.__enclos_env__$private$key_set(keyring_pwd_2)
+  expect_true(kb$keyring_is_locked())
+
+  expect_silent(kb$keyring_unlock(password = keyring_pwd_1))
   expect_silent(kb$keyring_delete())
 })
