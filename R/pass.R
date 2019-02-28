d8a4ef150d7f38c0f088b75bbc13f6de931d3755
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-28 12:55:29 +0100

    Replace getPass with askpass

--- R/pass.R
+++ R/pass.R
@@ -1,10 +1,3 @@
-
-## The second prompt is to work around a getPass bug
-
 get_pass <- function(prompt = "Password: ") {
-  res <- getPass::getPass(msg = prompt)
-  if (is.null(res)) {
-    res <- getPass::getPass(msg = prompt)
-  }
-  res
+  askpass::askpass(prompt = prompt)
 }
