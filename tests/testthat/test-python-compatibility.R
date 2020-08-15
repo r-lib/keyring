d0cc535344a4ecf173c2eb2aae7fe25522b3b196
Alec Wong <aw685@cornell.edu>
2020-08-14 22:27:17 -0400

    tests: update python compat. tests, add templates for later use

--- tests/testthat/test-python-compatibility.R
+++ tests/testthat/test-python-compatibility.R
@@ -8,7 +8,7 @@ pyring <- reticulate::import('keyring')
 
 context("Testing compatibility with python keyring package")
 
-test_that("Setting key with R can be read using python", {
+test_that("Setting key with R cannot be read using python under default settings (encoding mismatches)", {
   # Set with R, default settings
   keyring::key_set_with_value(service = "testService", username = "testUser", password = "test123")
   keyring::key_get(service = "testService", username = "testUser")
@@ -17,10 +17,71 @@ test_that("Setting key with R can be read using python", {
   expect_error(pyring$get_password(":testService:testUser", username = "testUser"))
 })
 
-test_that("Setting key with python can be read using R (reverse of previous)", {
+test_that("Setting key with python can be read using R", {
   # Python sets keys UTF-16LE encoded by default, but the R keyring package can handle that.
   pyring$set_password(":testPython:testUser", "testUser", "test123")
   expect_equal(keyring::key_get(service = "testPython", username = "testUser"), 'test123')
+})
+
+test_that("Reading UTF-16LE with encoding specified as UTF-8 will fail", {
+  # This shows that the encoding options are working, and being applied. The
+  # repeated lines below use various casing to ensure that casing doesn't
+  # matter.
+  encodings = c("UTF-8", "uTF-8", "UTf-8")
+
+  test_env_encoding <- function(encoding) {
+    Sys.setenv("KEYRING_ENCODING_WINDOWS" = encoding)
+    keyring::key_get(service = "testPython", username = "testUser")
+  }
+
+  for (e in encodings) {
+    expect_error(test_env_encoding(e))
+  }
+
+  # Reset
+  Sys.setenv("KEYRING_ENCODING_WINDOWS" = "")
+  # Use system options now.
+  test_opt_encoding <- function(encoding) {
+    options(keyring.encoding.windows = encoding)
+    keyring::key_get(service = "testPython", username = "testUser")
+  }
+
+  for (e in encodings) {
+    expect_error(test_opt_encoding(e))
+  }
+
+  # Reset
+  options(keyring.encoding.windows = NULL)
+
+})
+
+test_that("Reading UTF-16LE with encoding specified as UTF-16LE will work", {
+  # This shows that the encoding options are working, and being applied. The
+  # repeated lines below use various casing to ensure that casing doesn't
+  # matter.
+  encodings = c("UTF-16LE", "uTF-16LE", "UTf16le", 'utf-16le')
+
+  test_env_encoding <- function(encoding) {
+    Sys.setenv("KEYRING_ENCODING_WINDOWS" = encoding)
+    keyring::key_get(service = "testPython", username = "testUser")
+  }
+
+  for (e in encodings) {
+    expect_equal(test_env_encoding(e), 'test123')
+  }
+
+  # Reset
+  Sys.setenv("KEYRING_ENCODING_WINDOWS" = "")
+  # Use system options now.
+  test_opt_encoding <- function(encoding) {
+    options(keyring.encoding.windows = encoding)
+    keyring::key_get(service = "testPython", username = "testUser")
+  }
+
+  for (e in encodings) {
+    expect_equal(test_env_encoding(e), 'test123')
+  }
+
 })
 
 test_that("Any arbitrary key set by python can be read using R, not necessarily of the same form of this keyring API", {
@@ -40,3 +101,5 @@ test_that("Python can read from UTF-16LE encoded key set with R", {
 test_that("R can read from UTF-16LE encoded key set with R", {
   # R should also still be able to read from this.
 })
+
+key_delete(service = "testService", username = "testUser")
