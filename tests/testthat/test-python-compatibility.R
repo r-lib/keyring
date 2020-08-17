ade3b6a03ea323d31b38c5195cc3c82114c86290
Alec Wong <aw685@cornell.edu>
2020-08-16 20:37:16 -0400

    Style python test with tidyverse style

--- tests/testthat/test-python-compatibility.R
+++ tests/testthat/test-python-compatibility.R
@@ -10,11 +10,11 @@ library(keyring)
 
 context("Testing compatibility with python keyring package")
 
-KEYRING_ENV = 'keyring37'
-ENVS = reticulate::conda_list()$name
+KEYRING_ENV <- "keyring37"
+ENVS <- reticulate::conda_list()$name
 
-skip_if_no_conda = function() {
-  if (! reticulate::py_available()) {
+skip_if_no_conda <- function() {
+  if (!reticulate::py_available()) {
     skip("Python not available")
   }
   if (!(KEYRING_ENV %in% ENVS)) {
@@ -24,8 +24,8 @@ skip_if_no_conda = function() {
 }
 
 if (KEYRING_ENV %in% ENVS) {
-  reticulate::use_condaenv('keyring37', required = TRUE)
-  pyring <- reticulate::import('keyring')
+  reticulate::use_condaenv("keyring37", required = TRUE)
+  pyring <- reticulate::import("keyring")
 }
 
 test_that("Setting key with R cannot be read using python under default settings (encoding mismatches)", {
@@ -46,7 +46,7 @@ test_that("Setting key with python can be read using R", {
   skip_if_no_conda()
   # Python sets keys UTF-16LE encoded by default, but the R keyring package can handle that.
   pyring$set_password(":testPython:testUser", "testUser", "test123")
-  expect_equal(keyring::key_get(service = "testPython", username = "testUser"), 'test123')
+  expect_equal(keyring::key_get(service = "testPython", username = "testUser"), "test123")
 })
 
 test_that("Reading UTF-16LE with encoding specified as UTF-8 will fail", {
@@ -56,7 +56,7 @@ test_that("Reading UTF-16LE with encoding specified as UTF-8 will fail", {
   # This shows that the encoding options are working, and being applied. The
   # repeated lines below use various casing to ensure that casing doesn't
   # matter.
-  encodings = c("UTF-8", "uTF-8", "UTf-8")
+  encodings <- c("UTF-8", "uTF-8", "UTf-8")
 
   test_env_encoding <- function(encoding) {
     Sys.setenv("KEYRING_ENCODING_WINDOWS" = encoding)
@@ -81,7 +81,6 @@ test_that("Reading UTF-16LE with encoding specified as UTF-8 will fail", {
 
   # Reset
   options(keyring.encoding.windows = NULL)
-
 })
 
 test_that("Reading UTF-16LE with encoding specified as UTF-16LE will work", {
@@ -91,7 +90,7 @@ test_that("Reading UTF-16LE with encoding specified as UTF-16LE will work", {
   # This shows that the encoding options are working, and being applied. The
   # repeated lines below use various casing to ensure that casing doesn't
   # matter.
-  encodings = c("UTF-16LE", "uTF-16LE", "UTf16le", 'utf-16le')
+  encodings <- c("UTF-16LE", "uTF-16LE", "UTf16le", "utf-16le")
 
   test_env_encoding <- function(encoding) {
     Sys.setenv("KEYRING_ENCODING_WINDOWS" = encoding)
@@ -99,7 +98,7 @@ test_that("Reading UTF-16LE with encoding specified as UTF-16LE will work", {
   }
 
   for (e in encodings) {
-    expect_equal(test_env_encoding(e), 'test123')
+    expect_equal(test_env_encoding(e), "test123")
   }
 
   # Reset
@@ -111,9 +110,8 @@ test_that("Reading UTF-16LE with encoding specified as UTF-16LE will work", {
   }
 
   for (e in encodings) {
-    expect_equal(test_env_encoding(e), 'test123')
+    expect_equal(test_env_encoding(e), "test123")
   }
-
 })
 
 test_that("Any arbitrary key set by python can be read using R, not necessarily of the same form of this keyring API", {
@@ -122,18 +120,18 @@ test_that("Any arbitrary key set by python can be read using R, not necessarily 
   skip_if_no_conda()
   pyring$set_password("testPython", "testUser", "test123")
   pass <- keyring:::b_wincred_i_get(target = "testPython")
-  expect_equal(iconv(list(pass), from = 'UTF-16LE', to = ''), "test123")
+  expect_equal(iconv(list(pass), from = "UTF-16LE", to = ""), "test123")
 })
 
 test_that("Python can read from UTF-16LE encoded key set with R", {
   skip_if_not_win()
   skip_on_cran()
   skip_if_no_conda()
-  Sys.setenv("KEYRING_ENCODING_WINDOWS" = 'utf-16le')
+  Sys.setenv("KEYRING_ENCODING_WINDOWS" = "utf-16le")
   keyring::key_set_with_value(service = "testEncoding", username = "testUser", password = "test123")
   # Python should be able to read from this.
-  expect_equal(pyring$get_password(service_name = ":testEncoding:testUser", username = "testUser"), 'test123')
-  Sys.setenv("KEYRING_ENCODING_WINDOWS" = '')
+  expect_equal(pyring$get_password(service_name = ":testEncoding:testUser", username = "testUser"), "test123")
+  Sys.setenv("KEYRING_ENCODING_WINDOWS" = "")
 })
 
 if (interactive()) {
@@ -141,16 +139,16 @@ if (interactive()) {
     skip_if_not_win()
     skip_on_cran()
     skip_if_no_conda()
-    Sys.setenv("KEYRING_ENCODING_WINDOWS" = 'utf-16le')
+    Sys.setenv("KEYRING_ENCODING_WINDOWS" = "utf-16le")
     keyring::keyring_create("testKeyring")
-    keyring::key_set_with_value(service = "testEncoding", username = "testUser", password = "test123", keyring = 'testKeyring')
-    expect_equal(keyring::key_get(service = "testEncoding", username = "testUser", keyring = 'testKeyring'), 'test123')
-    Sys.setenv("KEYRING_ENCODING_WINDOWS" = '')
+    keyring::key_set_with_value(service = "testEncoding", username = "testUser", password = "test123", keyring = "testKeyring")
+    expect_equal(keyring::key_get(service = "testEncoding", username = "testUser", keyring = "testKeyring"), "test123")
+    Sys.setenv("KEYRING_ENCODING_WINDOWS" = "")
   })
 }
 
 # Clean up ---------------------------------------------------------------------
-try (
+try(
   {
     key_delete(service = "testService", username = "testUser")
     key_delete(service = "testEncoding", username = "testUser")
