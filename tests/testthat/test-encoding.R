6e099a3a4fca94c8764aa3f0a4efc85610f0d60c
Alec Wong <aw685@cornell.edu>
2020-08-16 21:31:58 -0400

    Update test encoding; add keyring test as well

--- tests/testthat/test-encoding.R
+++ tests/testthat/test-encoding.R
@@ -85,3 +85,28 @@ test_that("Set key with UTF-16LE encoding", {
   Sys.setenv("KEYRING_ENCODING_WINDOWS" = "")
   key_delete(service = SERVICE, username = USER)
 })
+
+test_that("Set key with UTF-16LE encoding plus a keyring", {
+  Sys.setenv("KEYRING_ENCODING_WINDOWS" = "utf-16le")
+  keyring <- random_keyring()
+  kb <- backend_wincred$new(keyring = keyring)
+  kb$.__enclos_env__$private$keyring_create_direct(keyring, "secret123!")
+  expect_true(keyring %in% kb$keyring_list()$keyring)
+
+  list <- kb$list()
+  expect_equal(nrow(list), 0)
+
+  service  <- "testService"
+  username <- "testUser"
+  password <- random_password()
+
+  expect_silent(
+    kb$set_with_value(service, username, password)
+  )
+
+  expect_equal(kb$get(service, username), password)
+  expect_silent(kb$delete(service, username))
+  expect_silent(kb$keyring_delete(keyring = keyring))
+  expect_false(keyring %in% kb$keyring_list()$keyring)
+  Sys.setenv("KEYRING_ENCODING_WINDOWS" = "")
+})
